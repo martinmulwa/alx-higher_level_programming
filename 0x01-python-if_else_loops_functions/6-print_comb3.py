a69d267319a2b543a7389f8f8a8eb6f8ce7135e1
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-06-01 12:45:09 +0300

    edit task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,9 +2,7 @@
 
 for i in range(0, 10):
     for j in range(i + 1, 10):
-        if (i != 0) or (j != 1):
-            print(", {:02d}".format(i * 10 + j), end="")
+        if i != 8:
+            print("{:d}{:d}".format(i, j), end=", ")
         else:
-            print("{:02d}".format(i * 10 + j), end="")
-
-print()
+            print("{:d}{:d}".format(i, j))
