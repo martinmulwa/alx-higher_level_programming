1b60356d50fd2a70787642db1ae3d29eab43a25f
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-06-04 12:12:34 +0300

    fix task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -10,5 +10,6 @@ def print_reversed_list_integer(my_list=[]):
     Return:
         None
     """
-    for i in reversed(my_list):
+    my_list.reverse()
+    for i in my_list:
         print("{:d}".format(i))
