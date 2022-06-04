7200ecf109a74222154a8387275884be4c6801d0
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-06-04 10:56:03 +0300

    update task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -9,6 +9,6 @@ def element_at(my_list, idx):
         idx: integer
 
     Return:
-        None
+        Element at position idx in my_list. Otherwise None
     """
     return None if (idx < 0 or idx >= len(my_list)) else my_list[idx]
