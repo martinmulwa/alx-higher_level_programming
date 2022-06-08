82e854be6f4940f0c73fc87ced11815d7b9e551e
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-06-08 19:39:01 +0300

    add task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+    Finds the number of keys in a dictionary
+
+    Args:
+        a_dictionary: dictionary
+
+    Return: integer
+    """
+    return len(a_dictionary.keys())
