5ae5f8258f945f426b6b5ea5edc1813b64c792d1
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-06-20 23:15:41 +0300

    add task 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    """
+    Executes a function safely
+
+    Args:
+        fct: the function to be executed
+        args: arguments for fct
+
+    Return: the return value of fct if successful. Otherwise None
+    """
+    try:
+        return fct(*args)
+    except BaseException as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
