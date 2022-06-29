1291eda2631b23be0dd32653673f48697afabf8c
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-06-29 17:37:33 +0300

    fix task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,14 +10,15 @@ def add_integer(a, b=98):
     """
     Adds 2 integers
     """
-    if type(a) not in [int, float]:
+
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
 
-    if type(a) == float:
+    if type(a) is float:
         a = int(a)
-    if type(b) == float:
+    if type(b) is float:
         b = int(b)
 
     return a + b
