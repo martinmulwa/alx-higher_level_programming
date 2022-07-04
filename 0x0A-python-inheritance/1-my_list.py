6d38e94066d6944e81f6c2893e86a6f086193721
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-07-04 16:23:21 +0300

    fix task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,4 +7,6 @@ class MyList(list):
 
     def print_sorted(self):
         """prints the list, but sorted (ascending sort)"""
-        print(sorted(self))
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
