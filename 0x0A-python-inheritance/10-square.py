473b5c6741f6275bab32f65852b77b19aebd58c9
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-07-04 17:56:53 +0300

    add task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""10-square.py"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Implements a Square geometric shape"""
+
+    def __init__(self, size):
+        """Initializes width and height attributes of Square object"""
+        super().__init__(size, size)
