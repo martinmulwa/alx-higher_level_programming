fd97a7dde049bdb9d1dba5001102870d76a76a2f
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-07-04 17:22:14 +0300

    add task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""7-base_geometry.py"""
+
+
+class BaseGeometry:
+    """Implements a basic geometric shape"""
+
+    def area(self):
+        """Raises Exception with message 'area() is not implemented'"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the value of a given name
+
+        Args:
+            name: string
+            value: integer value of given name
+
+        Returns: None
+        """
+        # if value is not an integer
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        # if value is less or equal to 0
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
