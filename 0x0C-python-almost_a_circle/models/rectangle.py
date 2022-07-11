6d6b366cf6425226200dd77df20dfbf57705fe8d
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-07-11 19:32:51 +0300

    add task 1

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""rectangle module"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Defines the Rectangle class the inherits from Base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter for the width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for the x attribute"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for the y attribute"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
