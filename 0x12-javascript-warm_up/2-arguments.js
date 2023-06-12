f46fc216dd0c606d52fb02e8515fb3923ad8010b
Martin Pius <martinmulwa1711@gmail.com>
2023-06-12 23:26:52 +0300

    task: Update task 2 solution

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,13 @@
 
 // Get the command-line arguments
 const args = process.argv.slice(2);
+const numArgs = args.length;
 
 // Check if any commandline arguments were passed
-if (args.length > 0) {
+if (numArgs === 0) {
+  console.log('No argument');
+} else if (numArgs === 1) {
   console.log('Argument found');
 } else {
-  console.log('No argument');
+  console.log('Arguments found');
 }
