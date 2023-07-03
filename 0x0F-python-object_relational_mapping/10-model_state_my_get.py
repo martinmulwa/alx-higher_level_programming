dd82e6879414161a2bfa270458dd86274ad97878
Martin Pius <martinmulwa1711@gmail.com>
2023-07-04 02:04:22 +0300

    task: Update task 10 solution

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python3
-""" Script that lists all State objects that contain the letter 'a'
-    from the database hbtn_0e_6_usa.
+
+"""
+Prints the State object with the specified name from the hbtn_0e_6_usa database
 """
 
+# Standard Library imports
+import sys
+
+# Third-party imports
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Local application imports
+from model_state import Base, State
+
 if __name__ == '__main__':
-    # Import necessary modules
-    import sys
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-
-    # Import the Base and State classes from the model_state module
-    from model_state import Base, State
-
-    # Connect to the MySQL database using the given command line arguments
+    # Create a database engine using the provided credentials
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-    # Create a session object using the connection
+    # Create a session factory using the database engine
     Session = sessionmaker(bind=engine)
+
+    # Create a session instance
     session = Session()
 
-    # Retrieve all State objects that contain the letter 'a' from the database
-    states_with_a = session.query(State)\
-        .filter(State.name.like('%a%'))\
-        .order_by(State.id)
+    # Get the user input and extract the name of the state
+    user_input = sys.argv[4].split("'")
+    state_name = user_input[0]
 
-    # Print the matching State objects to the console
-    for state in states_with_a:
-        print("{}: {}".format(state.id, state.name))
+    # Query the database for the state with the specified name
+    state = session.query(State).filter(State.name == state_name).first()
+
+    # If the state is found, print its ID
+    if state:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
